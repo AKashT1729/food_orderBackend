b85c7bc03fa0e1c9f50b3d5b992d896acc3249b3
Akash <thakare.akash007@gmail.com>
2025-10-18 13:20:34 +0530

    initialize backend with Express, CORS, and MongoDB connection

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectInstance = await mongoose.connect(
+      `${process.env.MONGODB_URL}/${DB_NAME}`
+    );
+    console.log(`MongoDB connected: ${connectInstance.connection.host}`);
+  } catch (error) {
+    console.log("MongoDB connecting error", error);
+    process.exit(1);
+  }
+};
+export default connectDB;
