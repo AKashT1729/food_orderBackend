82cf2443b705c264f0d344c0d47f9aff3b1483b3
Akash <thakare.akash007@gmail.com>
2025-10-27 15:37:16 +0530

    add product, order, and order item models

---
+++ src/models/product.models.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    product_id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    }
+});
+
+export const Product = mongoose.model('Product', productSchema);
