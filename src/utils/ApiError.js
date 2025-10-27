3de59963e340439b5be2ef0f8028fc4ce6bd74c5
Akash <thakare.akash007@gmail.com>
2025-10-27 12:57:33 +0530

    add cookie parser middleware and static file serving; implement database connection and error handling

---
+++ src/utils/ApiError.js
@@ -0,0 +1,23 @@
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    massage = "Something want wrong",
+    errors = [],
+    stack = ""
+  ) {
+    super(massage);
+    this.statusCode = statusCode;
+    this.errors = errors;
+    this.data = null;
+    this.message = massage;
+    this.success = false;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constrictor);
+    }
+  }
+}
+
+export { ApiError };
